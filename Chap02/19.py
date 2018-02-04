8cb6975de6e91589d77e0cbb933ba1a99dfd2d0f
Tetsuro Rintani <rquery.10@gmail.com>
2018-02-04 12:51:14 +0900

    19

--- Chap02/19.py
+++ Chap02/19.py
@@ -1,9 +1,9 @@
-from collection import Counter
+from collections import Counter
 
 with open("hightemp.txt") as f:
     lines = f.readlines()
     counter = Counter([line.split()[0] for line in lines])
 
     for word, count in counter.most_common():
-        print (word + ":\t" + count)
+        print (word + ":\t" + str(count))
     
