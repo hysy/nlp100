00feadd2249a1ece86dbb36d74746bab750a34dd
Tetsuro Rintani <rquery.10@gmail.com>
2018-02-05 18:04:31 +0900

    50 nado

---
+++ Chap06/ex50.py
@@ -0,0 +1,26 @@
+import re
+
+fn = 'nlp.txt'
+
+def get_sentence_list(plain):
+    sentence_reg = re.compile('([.;:?!])\s([A-Z])')
+    # 改行をスペースに
+    stream = re.sub('(\s*\n+)+\s*', ' ', plain)
+
+    # 区切り文字（文の最初の文字） + 区切り抜き文 +  区切り文字（文の最後の文字）
+    # 最初は検知されないのでダミーを付ける
+    tmp = []
+    tmp.append("")
+    for s in sentence_reg.split(stream):
+        tmp.append(s)
+    tmp.append("")
+    
+    return ["".join(tmp[3*i:3*(i+1)]) for i in range(len(tmp) // 3)]
+
+if __name__ == '__main__':
+
+    data = open(fn).read()
+    for line in get_sentence_list(data):
+        print(line)
+    
+        
